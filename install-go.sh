2b9cbb4207eebe0ee7819430b672d2d7b083662d
Jorge Rodriguez <me@jarodriguez.ca>
2021-11-20 18:30:01 -0500

    Run shellcheck

--- install-go.sh
+++ install-go.sh
@@ -6,15 +6,15 @@ set -euxo pipefail
 VERSION="1.17.3"
 
 # download new version
-echo "Downloading GO version $VERSION"
+echo "Downloading GO version ${VERSION}"
 
-wget "https://golang.org/dl/go$VERSION.linux-amd64.tar.gz"
+wget "https://golang.org/dl/go${VERSION}.linux-amd64.tar.gz"
 
 # remove old version of go
 sudo rm -rf /usr/local/go
 
 # untar file
-sudo tar -C /usr/local -xzf "go$VERSION.linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf "go${VERSION}.linux-amd64.tar.gz"
 
 # remove download file
-rm "go$VERSION.linux-amd64.tar.gz"
+rm "go${VERSION}.linux-amd64.tar.gz"
