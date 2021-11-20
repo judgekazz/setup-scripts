e5c8b02998240c2ff8935cecba0ddc6a7b891274
Jorge Rodriguez <me@jarodriguez.ca>
2021-11-20 18:21:23 -0500

    Create script to install golang in linux

---
+++ install-go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# adaptation of install instructions on https://golang.org/doc/install
+
+set -euxo pipefail
+
+VERSION="1.17.3"
+
+# download new version
+echo "Downloading GO version $VERSION"
+
+wget "https://golang.org/dl/go$VERSION.linux-amd64.tar.gz"
+
+# remove old version of go
+sudo rm -rf /usr/local/go
+
+# untar file
+sudo tar -C /usr/local -xzf "go$VERSION.linux-amd64.tar.gz"
+
+# remove download file
+rm "go$VERSION.linux-amd64.tar.gz"
